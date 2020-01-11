76cc740c70b1b865919925992c6e63c69dd5c471
zhubo <670403684@qq.com>
2020-01-11 11:23:44 +0800

    commit

---
+++ src/main/java/com/github/tc/Agent.java
@@ -0,0 +1,20 @@
+package com.github.tc;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: zhubo
+ * @description:
+ * @time: 2020年01月11日
+ * @modifytime:
+ */
+public class Agent {
+
+    public static void premain(String agentArgs, Instrumentation instrumentation) {
+
+
+
+    }
+}
